1bdc200d7e64a9e4efea4ad2818746049dd62441
duarrrd <duarrrd88@gmail.com>
2024-05-29 15:32:28 +0300

    Multiple markers

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import uuid
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from folium.features import DivIcon
 
 app = Flask(__name__)
 
@@ -22,7 +23,7 @@ AWS_QUERYSTRING_AUTH = False
 # Initialize boto3 client for S3
 s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION, endpoint_url=AWS_S3_ENDPOINT_URL)
 
-def create_map(latitude, longitude, radius, icon, labels_in_local_lang):
+def create_map(latitude, longitude, radius, icon, labels_in_local_lang, text, markers):
     # Calculate the zoom level
     zoom_start = 16 - math.log(radius*2/1000)/math.log(2)
 
@@ -42,11 +43,68 @@ def create_map(latitude, longitude, radius, icon, labels_in_local_lang):
         attr='MapTiler'
     )
 
+    # Map the icon to the correct string
+    icon_mapping = {
+        "car": "car",
+        "boat": "ship",
+        "yacht": "sailboat",
+        "seaplane": "plane-departure",
+        "plane": "plane",
+        "ferry": "ferry",
+        "resort-hotel": "ranking-star",
+        "hotel": "bell-concierge",
+    }
+    icon = icon_mapping.get(icon, "car")
+
     # Add a marker at the center
     folium.Marker(
         location=[latitude, longitude],
         icon=folium.Icon(icon=icon, prefix='fa'),
     ).add_to(m)
+
+    # Add a label that will always be displayed
+    folium.map.Marker(
+        [latitude, longitude],
+        icon=DivIcon(
+            icon_size=(150,36),
+            icon_anchor=(0,0),
+            html='<div style="font-size: 10pt; '
+                 'color: white; '
+                 'background-color: rgba(0, 0, 0, 0.7); '
+                 'padding: 6px; '
+                 'box-shadow: rgba(0, 0, 0, 0.4) 5px 5px, rgba(0, 0, 0, 0.3) 10px 10px, rgba(0, 0, 0, 0.2) 15px 15px, rgba(0, 0, 0, 0.1) 20px 20px, rgba(0, 0, 0, 0.05) 25px 25px; '
+                 'border-radius: 0 100px 100px 100px;'
+                 'display: inline-block;'
+                 'font-weight: bold;'
+                 f'font-style: oblique;">{text}</div>',
+            )
+        ).add_to(m)
+
+    # Add additional markers
+    for marker in markers:
+        marker_icon = icon_mapping.get(marker['icon'], "car")
+        folium.Marker(
+            location=[marker['latitude'], marker['longitude']],
+            icon=folium.Icon(icon=marker_icon, prefix='fa'),
+        ).add_to(m)
+        # Add a label that will always be displayed
+        folium.map.Marker(
+            location=[marker['latitude'], marker['longitude']],
+            icon=DivIcon(
+                icon_size=(150, 36),
+                icon_anchor=(0, 0),
+                html='<div style="font-size: 10pt; '
+                     'color: white; '
+                     'background-color: rgba(0, 0, 0, 0.7); '
+                     'padding: 6px; '
+                     'box-shadow: rgba(0, 0, 0, 0.4) 5px 5px, rgba(0, 0, 0, 0.3) 10px 10px, rgba(0, 0, 0, 0.2) 15px 15px, rgba(0, 0, 0, 0.1) 20px 20px, rgba(0, 0, 0, 0.05) 25px 25px; '
+                     'border-radius: 0 100px 100px 100px;'
+                     'display: inline-block;'
+                     'font-weight: bold;'
+                     f'font-style: oblique;">{marker["text"]}</div>',
+            )
+        ).add_to(m)
+
     # Add a circle to the map
     folium.Circle(
         location=[latitude, longitude],
@@ -55,6 +113,7 @@ def create_map(latitude, longitude, radius, icon, labels_in_local_lang):
         fill=True,
         fill_color='green'
     ).add_to(m)
+
     # Remove the attribution text
     m.get_root().html.add_child(folium.Element("<style>.leaflet-control-attribution {display: none;}</style>"))
 
@@ -82,7 +141,13 @@ def create_picture_of_a_map():
     os.remove(map_html_path)
     return filename
 
-@app.route('/map-picture', methods=['GET'])
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({'status': 'success', 'message': 'pong'})
+
+
+@app.route('/map-picture', methods=['POST'])
 def get_map_picture():
     data = request.get_json()
     latitude = data['latitude']
@@ -90,20 +155,11 @@ def get_map_picture():
     radius = data['radius']
     icon = data['icon']
     labels_in_local_lang = data.get('labels-in-local-lang', False)
+    text = data['text']
+    markers = data.get('markers', [])
     radius *= 1000  # To turn KM into M
 
-    # Map the icon to the correct string
-    icon_mapping = {
-        "car": "car",
-        "boat": "ship",
-        "yacht": "sailboat",
-        "seaplane": "plane-departure",
-        "plane": "plane",
-        "ferry": "ferry"
-    }
-    icon = icon_mapping.get(icon, "car")
-
-    create_map(latitude, longitude, radius, icon, labels_in_local_lang)
+    create_map(latitude, longitude, radius, icon, labels_in_local_lang, text, markers)
     filename = create_picture_of_a_map()
 
     with open(filename, 'rb') as data:
